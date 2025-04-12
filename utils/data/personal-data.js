94d2dd41c6292fd7e9cd1a1f91d780638f31aa30
abhishek M <abhishek.mourya@amuratech.com>
2025-04-12 23:14:00 +0530

    Updated Resume

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -3,12 +3,12 @@ export const personalData = {
   profile: "/profile.jpg",
   designation: "Full Stack Developer",
   description:
-    "I'm Abhishek Mourya, a seasoned professional in programming, specializing in frontend development. While I have experience across the MERN stack, my expertise shines brightest in crafting engaging user interfaces and delightful user experiences. With two years of industry experience, I bring a wealth of knowledge in frontend technologies. I thrive on problem-solving and continuously seek to expand my skills to stay ahead of the curve. JavaScript is my forte, and I'm committed to leveraging my frontend expertise to drive innovation in web application development.",
+    "I'm Abhishek Mourya, a seasoned professional in programming, specializing in frontend development. While I have experience across the MERN stack, my expertise shines brightest in crafting engaging user interfaces and delightful user experiences. With three years of industry experience, I bring a wealth of knowledge in frontend technologies. I thrive on problem-solving and continuously seek to expand my skills to stay ahead of the curve. JavaScript is my forte, and I'm committed to leveraging my frontend expertise to drive innovation in web application development.",
   email: "abhimourya9325@gmail.com",
   phone: "9923266920",
   address: "Pune, Maharashtra, India",
   github: "https://github.com/Abhishek-9325",
   linkedIn: "https://www.linkedin.com/in/abhishek-mourya-b48a27289",
   resume:
-    "https://drive.google.com/file/d/1XuahJyzJmRaXBfsxLa7HhloU1WKBUfCx/view?usp=sharing",
+    "https://drive.google.com/file/d/15hzueA6x-IEQuZppCUfYZVWG2O0Wjt6e/view?usp=sharing",
 };
